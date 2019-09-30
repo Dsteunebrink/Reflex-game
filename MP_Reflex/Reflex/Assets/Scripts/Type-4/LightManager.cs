34df5841664c0fd8d50431d5af2eb0286b8a37f3
DESKTOP-CTA26N5\Dani Steunebrink <danisteunebrink@live.nl>
2019-09-30 09:33:43 +0200

    scripts made and materials made

---
+++ MP_Reflex/Reflex/Assets/Scripts/Type-4/LightManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+
+    public List<GameObject> lights;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
