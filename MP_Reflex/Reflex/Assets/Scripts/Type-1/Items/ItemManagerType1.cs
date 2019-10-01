e855d13338f62a5cca9819cd19c52ed9445a740d
DESKTOP-CTA26N5\Dani Steunebrink <danisteunebrink@live.nl>
2019-10-01 11:21:43 +0200

    Type-1 scripts documented

--- MP_Reflex/Reflex/Assets/Scripts/Type-1/Items/ItemManagerType1.cs
+++ MP_Reflex/Reflex/Assets/Scripts/Type-1/Items/ItemManagerType1.cs
@@ -4,27 +4,34 @@ using UnityEngine;
 
 public class ItemManagerType1 : MonoBehaviour
 {
+    //Variables used in script: item prefab used for spawn, clone gameobject used to change things with instantiated objects and Vector3 randPos to get a random position for the clone.
     public GameObject item;
     private GameObject clone;
     private Vector3 randPos;
     
+    //Boolean to set for when the itemSpawned
     private bool itemSpawned;
 
+    //Timer to check when to spawn the next clone. oldtime to check how long it took to spawn the last clone. Addtime to add to the timer so the clone spawns faster everytime.
     private float timer;
     public float oldTime = 0.0f;
     private float addTime = 0.2f;
 
+    //Healthmanager to change the health of the player.
     private HealthManager healthMan;
 
     void Start () {
+        //Find healthmanager in scene and reset gravity.
         healthMan = GameObject.Find ("HealthManager").GetComponent<HealthManager> ();
         Physics.gravity = new Vector3 (0f, -9.8f, 0f);
     }
 
     private void Update () {
 
+        //Start timer.
         timer += Time.deltaTime;
 
+        //Check timer to spawn clone.
         if (timer > 6) {
 
             StartCoroutine (ItemDrop ());
@@ -38,14 +45,18 @@ public class ItemManagerType1 : MonoBehaviour
     }
 
     IEnumerator ItemDrop () {
+        //Check if there are too many items in the scene.
         if (GameObject.FindGameObjectsWithTag ("Item").Length < 10) {
 
+            //make addtime lesser if the last one is already spawning fast.
             if (oldTime == 4.4f) {
                 addTime = 0.1f;
             }
 
+            //make a random pos.
             randPos = Camera.main.ViewportToWorldPoint (new Vector3 (Random.Range (0.1f, 0.9f), Random.Range (0.1f, 0.9f), 10f));
             
+            //Spawn item  on random pos
             clone = Instantiate (item, randPos, Quaternion.identity);
             clone.transform.position = new Vector3 (clone.transform.position.x, -6f, 10f);
              
@@ -54,6 +65,7 @@ public class ItemManagerType1 : MonoBehaviour
     }
 
     private void OnTriggerEnter (Collider other) {
+        //destroy item if it fell out of camera view and do minushelath because player missed it.
         if (other.CompareTag ("Item")) {
             healthMan.MinusHealth (1);
             Destroy (other.gameObject);
--- MP_Reflex/Reflex/Assets/Scripts/Type-1/Items/ItemVel.cs
+++ MP_Reflex/Reflex/Assets/Scripts/Type-1/Items/ItemVel.cs
@@ -5,10 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class ItemVel : MonoBehaviour
 {
+    //Variable for velocity and the timer of the manager script.
     private int vel;
-    private Vector3 angVel = new Vector3(10, 0, 0);
     private ItemManagerType1 manTime;
 
+    //Boolean to set wich side the item has to move. and when the choosing is done
     private bool right = false;
     private bool left = false;
     private bool caseDone = false;
@@ -16,16 +17,21 @@ public class ItemVel : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Check to see if this is the right scene for this script otherwise destroy it.
         if (SceneManager.GetActiveScene ().name != "Type-1") {
             Destroy (this.GetComponent<ItemVel> ());
         } else {
+            //Find manager in scene.
             manTime = GameObject.Find ("ItemManager").GetComponent<ItemManagerType1> ();
-
+            
+            //Give more velocity and stronger gravity when the player is surviving long.
             if (manTime.oldTime <= 3.0f) {
+                //Give random velocity to item.
                 vel = Random.Range (10, 12);
                 this.GetComponent<Rigidbody> ().velocity = vel * transform.localScale.y * this.transform.up;
             } else if (manTime.oldTime <= 4.0f) {
                 vel = Random.Range (14, 18);
+                //Make the gravity stronger.
                 Physics.gravity = new Vector3 (0f, -20.0f, 0f);
                 this.GetComponent<Rigidbody> ().velocity = vel * transform.localScale.y * this.transform.up;
             } else {
@@ -37,20 +43,14 @@ public class ItemVel : MonoBehaviour
     }
 
     private void Update () {
-        
-        /*if (caseDone == false) {
-            switch (Random.Range (0, 2)) {
-                case 0: right = true; break;
-                case 1: left = true; break;
-            }
-        }*/
-
+        //Choose based on the starting position wich way the item should move right or left.
         if(this.transform.position.x <= 5.61 && caseDone == false) {
             right = true;
         } else if (this.transform.position.x >= 5.61 && caseDone == false) {
             left = true;
         }
 
+        //Make the item move either right or left based on the booleans.
         if (right == true) {
             transform.position += transform.right * Time.deltaTime;
             caseDone = true;
