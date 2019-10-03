fdcf24f9d5334af632192f577b33afbb43610974
DESKTOP-CTA26N5\Dani Steunebrink <danisteunebrink@live.nl>
2019-10-03 10:03:23 +0200

    pause menu script

--- MP_Reflex/Reflex/Assets/Scripts/UI/PauseMenu.cs
+++ MP_Reflex/Reflex/Assets/Scripts/UI/PauseMenu.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
+    public GameObject pauseMenuPanel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,25 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown (KeyCode.Escape) && pauseMenuPanel.activeInHierarchy == false) {
+            pauseMenuPanel.SetActive (true);
+        } else if (Input.GetKeyDown (KeyCode.Escape) && pauseMenuPanel != null) {
+            pauseMenuPanel.SetActive (false);
+        }
+    }
+
+    public void Options () {
+        //Get options panel.
+    }
+
+    public void Retry () {
+        //When the retry is clicked reset scene.
+        Time.timeScale = 1;
+        SceneManager.LoadSceneAsync (SceneManager.GetActiveScene ().name);
+    }
+
+    public void Quit () {
+        //Load main menu scene.
+        SceneManager.LoadSceneAsync ("Main_Menu");
     }
 }
